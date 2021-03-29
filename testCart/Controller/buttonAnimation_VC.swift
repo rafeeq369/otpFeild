8b7564957e4cd14e8967416f11d7194d85b178ff
Rafeeq <rafeeqrahman1997@gmail.com>
2021-03-29 14:07:16 -0700

    nothing Special

--- testCart/Controller/buttonAnimation_VC.swift
+++ testCart/Controller/buttonAnimation_VC.swift
@@ -12,7 +12,7 @@ class buttonAnimation_VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func actBtn(_ button: TransitionButton) {
